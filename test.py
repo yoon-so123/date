e6a06a6be08e623f85c279a0e412305a5b8ee80c
yoon-so123 <dadada2659@gmail.com>
2025-05-22 14:12:38 +0900

    d
    d

---
+++ test.py
@@ -0,0 +1,35 @@
+# let a = 3; const b = 'hi';
+
+# a=3
+# b = 'hi'
+# c = True
+# d = False
+
+# print("hello, python!"+b) #js : 문자 + 숫자 = 문자
+# #python 은 문자+숫자는 에러 뜹니다.
+# print('bye') #console.log("hi");
+# print('bye'+ str(a))
+
+# item_name 변수를 만들고 widget 이라는 값 저장
+# price 변수를 만들고 23.5 저장 (숫자)
+# stock 변수를 만들고 100 저장(숫자)
+# is_empty 변수를 만들고 거짓 저장(불)
+# item_name 값 띄우고 가격 띄우고 stock 출력
+# 예: widget 23.5 100 출력되면됨.
+
+# item_name = 'widget'
+# price = 23.5
+# stock = 100
+# is_empty = False
+
+# print(item_name, price, stock)
+
+
+# name = input() 
+# print(f'안녕하세요. {name}님')
+
+#let myArray = [1,2,3,4,5,];
+myList = [1,2,3,4,5,]
+print(myList[1], myList[-1])
+#myArray.length
+print(len(myList))
